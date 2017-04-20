04f749ea56eaf2676c334f044325bd01dd7b0dd6
Erik Onarheim <erik.onarheim@gmail.com>
2017-04-19 22:32:08 -0500

    lul wut

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ echo "Compiling TSC"
 npm run build
 
 echo "Copying built files"
-cp -R $(ls | grep -v '^\(out\|excalibur\)$') out
+cp -R $(ls | grep -v '^\(out\|lib\\excalibur\|\.vscode)$') out
 cd out
 
 echo "Setting commit number ${CURRENT_COMMIT}"
