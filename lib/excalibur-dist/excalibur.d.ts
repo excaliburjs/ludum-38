1130a09caa5c8c237667ec31b6282d208d3751d4
Erik Onarheim <erik.onarheim@gmail.com>
2017-04-19 20:56:38 -0500

    Include excalibur dist in check in :ghost:

---
+++ lib/excalibur-dist/excalibur.d.ts
@@ -0,0 +1,9 @@
+/*! excalibur - vlocal - 2017-04-19
+* https://github.com/excaliburjs/Excalibur
+* Copyright (c) 2017 Excalibur.js <https://github.com/excaliburjs/Excalibur/graphs/contributors>; Licensed BSD-2-Clause
+* @preserve */
+/// <reference path="excalibur.amd.d.ts" />
+
+// Expose all public API as ex namespace
+export * from 'Index';
+export as namespace ex;
