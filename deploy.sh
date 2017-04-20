d3923a6cea13dadb8c33bcbd56a41201fa5b0012
Erik Onarheim <erik.onarheim@gmail.com>
2017-04-19 22:06:40 -0500

    More magic

--- deploy.sh
+++ deploy.sh
@@ -34,6 +34,7 @@ git add -A
 git commit --allow-empty -m "Deploying game for $CURRENT_COMMIT" || exit 1
 git checkout gh-pages
 git merge master --squash 
+git commit --allow-empty -m "Deploying game for $CURRENT_COMMIT" || exit 1
 git push origin gh-pages > /dev/null 2>&1 || exit 1
 
 echo "Pushed deployment successfully"
