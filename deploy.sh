e167a161cf62c6868ce678a19ccf6a684e69d808
Erik Onarheim <erik.onarheim@gmail.com>
2017-04-19 22:25:54 -0500

    Lol :shipit:

--- deploy.sh
+++ deploy.sh
@@ -8,33 +8,25 @@ npm install typescript@2.2.2 -g
 
 echo "Cloning master branch..."
 
-git clone -b master "https://${GH_TOKEN}@${GH_REF}" out > /dev/null 2>&1 || exit 1
+git clone -b gh-pages "https://${GH_TOKEN}@${GH_REF}" out > /dev/null 2>&1 || exit 1
 
-cd out
-git fetch origin gh-pages
+rm -r out/*
 
 echo "Compiling TSC"
 
 npm run build
 
+echo "Copying built files"
+cp -R $(ls | grep -v '^\(out\|Excalibur\)$') out
+cd out
+
 echo "Setting commit number ${CURRENT_COMMIT}"
 sed -i "s/COMMIT_NUMBER/${CURRENT_COMMIT}/g" ./index.html
-
-echo "Building new .gitignore"
-rm .gitignore
-echo "node_modules/" > .gitignore
-echo "**.ts" >> .gitignore
-echo "**.yml" >> .gitignore
-echo "**.sh" >> .gitignore
-cat .gitignore
 
 echo "Committing and pushing to GH"
 git config user.name "Travis-CI"
 git config user.email "travis@excaliburjs.com"
 git add -A
-git commit --allow-empty -m "Placeholder" || exit 1
-git checkout gh-pages
-git merge master --squash 
 git commit --allow-empty -m "Deploying game for $CURRENT_COMMIT" || exit 1
 git push origin gh-pages > /dev/null 2>&1 || exit 1
 
