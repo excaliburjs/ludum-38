40f4ed876f27002784108e3f04966759f5011c40
Alan Grgic <alan.grgic@gmail.com>
2017-04-22 16:03:15 -0500

    fix config file

--- game/Config.ts
+++ game/Config.ts
@@ -14,17 +14,9 @@ var Config = {
    enemyRayCastAngle: Math.PI / 4,
    enemyRayLength: 200,
    enemyRayCount: 5,
-
-<<<<<<< HEAD
    foodWidth: 48,
    foodHeight: 48,
-   foodSpawnCount: 4
-=======
-   foodWidth: 100,
-   foodHeight: 100,
    foodSpawnCount: 4,
->>>>>>> 758ba0d10787ea6c9d0c77300208a0923b90dd13
-
    soundVolume: 1,
    backgroundVolume: 0.1
 }
