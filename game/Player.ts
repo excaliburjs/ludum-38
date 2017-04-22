f0b8c9e7794003a9c09ac6920ff36d44ad35e4e1
Josh Edeen <josh.edeen@gmail.com>
2017-04-22 11:42:22 -0500

    Enable diagonal player movement

--- game/Player.ts
+++ game/Player.ts
@@ -12,30 +12,28 @@ class Player extends ex.Actor {
    }
 
    public onInitialize(engine: ex.Engine) {
-       game.input.keyboard.on('hold', (keyHeld?: ex.Input.KeyEvent) => {
-            
-            switch(keyHeld.key) {
-               case ex.Input.Keys.Up :
-               case ex.Input.Keys.W :
-                  this.vel.setTo(0, -Config.playerVel)
-                  break;
-               case ex.Input.Keys.Down :
-               case ex.Input.Keys.S :
-                  this.vel.setTo(0, Config.playerVel)
-                  break;
-               case ex.Input.Keys.Left :
-               case ex.Input.Keys.A :
-                  this.vel.setTo(-Config.playerVel, 0)
-                  break;
-               case ex.Input.Keys.Right :
-               case ex.Input.Keys.D :
-                  this.vel.setTo(Config.playerVel, 0)
-                  break;
-               // default :
-               //     ex.Logger.getInstance().info('unknown key pressed');
-            }
-            
-        });
+      game.input.keyboard.on('hold', (keyHeld?: ex.Input.KeyEvent) => {
+
+         switch(keyHeld.key) {
+            case ex.Input.Keys.Up :
+            case ex.Input.Keys.W :
+               this.vel.setTo(this.vel.x, -Config.playerVel);
+               break;
+            case ex.Input.Keys.Down :
+            case ex.Input.Keys.S :
+               this.vel.setTo(this.vel.x, Config.playerVel);
+               break;
+            case ex.Input.Keys.Left :
+            case ex.Input.Keys.A :
+               this.vel.setTo(-Config.playerVel, this.vel.y);
+               break;
+            case ex.Input.Keys.Right :
+            case ex.Input.Keys.D :
+               this.vel.setTo(Config.playerVel, this.vel.y);
+               break;
+         }
+
+      });
 
       this.on('postupdate', (evt: ex.PostUpdateEvent) => {
          this.vel.setTo(0, 0);
