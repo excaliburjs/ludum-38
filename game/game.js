a3ed70bf14c0d0f1b57e3954c5cbdc5c7b5e34da
Travis-CI <travis@excaliburjs.com>
2017-04-22 14:03:54 +0000

    Deploying game for 8fa697ed52a6dbf168fc99fe4c60928a321119cb

--- game/game.js
+++ game/game.js
@@ -8,20 +8,95 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
-var Resources = {};
+var Resources = {
+    playerSheet: new ex.Texture('../img/player.png'),
+    foodSheet: new ex.Texture('../img/food.png')
+};
 var Config = {
     gameWidth: 800,
-    gameHeight: 600
+    gameHeight: 600,
+    playerStart: new ex.Vector(0, 0),
+    playerWidth: 50,
+    playerHeight: 50,
+    playerVel: 100,
+    foodWidth: 100,
+    foodHeight: 100
 };
 var Stats = (function () {
     function Stats() {
     }
     return Stats;
 }());
+/// <reference path="../lib/excalibur-dist/excalibur.d.ts" />
+/// <reference path="Resources.ts" />
+/// <reference path="Config.ts" />
+var Player = (function (_super) {
+    __extends(Player, _super);
+    /**
+     * Build the player for the game
+     */
+    function Player(x, y) {
+        var _this = _super.call(this, x, y, Config.playerWidth, Config.playerHeight) || this;
+        _this.addDrawing(Resources.playerSheet);
+        return _this;
+    }
+    Player.prototype.onInitialize = function (engine) {
+        var _this = this;
+        game.input.keyboard.on('hold', function (keyHeld) {
+            switch (keyHeld.key) {
+                case ex.Input.Keys.Up:
+                case ex.Input.Keys.W:
+                    _this.vel.setTo(0, -Config.playerVel);
+                    break;
+                case ex.Input.Keys.Down:
+                case ex.Input.Keys.S:
+                    _this.vel.setTo(0, Config.playerVel);
+                    break;
+                case ex.Input.Keys.Left:
+                case ex.Input.Keys.A:
+                    _this.vel.setTo(-Config.playerVel, 0);
+                    break;
+                case ex.Input.Keys.Right:
+                case ex.Input.Keys.D:
+                    _this.vel.setTo(Config.playerVel, 0);
+                    break;
+            }
+        });
+        this.on('postupdate', function (evt) {
+            _this.vel.setTo(0, 0);
+        });
+    };
+    return Player;
+}(ex.Actor));
+/// <reference path="../lib/excalibur-dist/excalibur.d.ts" />
+/// <reference path="Resources.ts" />
+/// <reference path="Config.ts" />
+var Food = (function (_super) {
+    __extends(Food, _super);
+    /**
+     *
+     */
+    function Food(x, y) {
+        var _this = _super.call(this, x, y, Config.foodWidth, Config.foodHeight) || this;
+        _this.addDrawing(Resources.foodSheet);
+        return _this;
+    }
+    return Food;
+}(ex.Actor));
+/// <reference path="./Player.ts" />
+/// <reference path="./Food.ts" />
 var ScnMain = (function (_super) {
     __extends(ScnMain, _super);
+    /**
+     * The main scene for the game
+     */
     function ScnMain() {
-        return _super !== null && _super.apply(this, arguments) || this;
+        var _this = _super.call(this) || this;
+        var player = new Player(Config.playerStart.x, Config.playerStart.y);
+        _this.add(player);
+        var food = new Food(100, 100);
+        _this.add(food);
+        return _this;
     }
     ScnMain.prototype.onInitialize = function (engine) {
     };
