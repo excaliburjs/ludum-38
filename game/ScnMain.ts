26b0b77309c36d854f3ee2905a544cc95ffb99e3
Erik Onarheim <erik.onarheim@gmail.com>
2017-04-22 09:11:41 -0500

    Fix Main Scene params

--- game/ScnMain.ts
+++ game/ScnMain.ts
@@ -6,8 +6,8 @@ class ScnMain extends ex.Scene {
    /**
     * The main scene for the game
     */
-   constructor() {
-      super();
+   constructor(engine: ex.Engine) {
+      super(engine);
       var player = new Player(Config.playerStart.x, Config.playerStart.y);
       this.add(player)
 
