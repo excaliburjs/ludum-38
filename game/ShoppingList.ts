126047a6586abea3f0a1610088bd57d9a99ec180
Alan Grgic <alan.grgic@gmail.com>
2017-04-22 11:56:34 -0500

    add shopping list implementation

---
+++ game/ShoppingList.ts
@@ -0,0 +1,23 @@
+
+ 
+
+class ShoppingList {
+    constructor(public items: Array<Food>) {
+
+    }
+
+    public removeItem(id: string) {
+       if(this.items && this.items.length){
+         var idxsToRemove = this.items.map(function(obj, index) {
+            if(obj.ShoppingListId == id) {
+               return index;
+            }
+         });
+
+         if(idxsToRemove && idxsToRemove.length){
+               this.items.splice(idxsToRemove[0], 1)
+         }
+       }
+
+    }  
+}
