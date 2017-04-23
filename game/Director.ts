2618d7062285c7f153c3a228ca6c36f8f1ca6cca
Josh Edeen <josh.edeen@gmail.com>
2017-04-23 11:30:46 -0500

    Add game's start timings

--- game/Director.ts
+++ game/Director.ts
@@ -1,4 +1,53 @@
 class Director extends ex.Actor {
 
+   private _showGroceryListTimer: ex.Timer;
+   private _spawnFoodTimer: ex.Timer;
+   private _spawnFirstEnemyTimer: ex.Timer;
 
+   public setup() {
+      ex.Logger.getInstance().info('director setup');
+
+      this._showGroceryListTimer = new ex.Timer(() => {
+         this._showGroceryList();
+      }, Config.groceryListTime);
+      scnMain.add(this._showGroceryListTimer);
+
+      this._spawnFoodTimer = new ex.Timer(() => {
+         this._spawnFood();
+      }, Config.spawnFoodTime);
+      scnMain.add(this._spawnFoodTimer);
+
+      this._spawnFirstEnemyTimer = new ex.Timer(() => {
+         this._spawnFirstEnemy();
+      }, Config.spawnFirstEnemyTime);
+      scnMain.add(this._spawnFirstEnemyTimer);
+   }
+
+   //1. start zoomed in on player, zoom out
+   // public zoomOut() {
+      // zooming is broken
+   // }
+
+   //2. display grocery list
+   private _showGroceryList() {
+      console.log('show grocery list');
+      //TODO
+   }
+
+
+   //3. spawn in food
+   private _spawnFood() {
+      console.log('spawn food');
+      //TODO
+   }
+
+   //4. the first antagonist arrives
+   private _spawnFirstEnemy() {
+      scnMain.spawnEnemy();
+   }
+
+   //4b. add more antagonists
+
+   //5. checkout - game ends
+   // 
 }
