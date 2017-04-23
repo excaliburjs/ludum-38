b065593fba4abe98adbc9a4237376285d8f30a02
Josh Edeen <josh.edeen@gmail.com>
2017-04-23 18:13:01 -0500

    Reduce palette size

--- game/Resources.ts
+++ game/Resources.ts
@@ -1,6 +1,6 @@
 var Resources = {
    map: new Extensions.Tiled.TiledResource('assets/map.json'),
-   playerSheet: new ex.Texture('img/player-sprite.png'),
+   playerSheet: new ex.Texture('img/player.png'),
    foodSheet: new ex.Texture('img/food.png'),
    enemySheet: new ex.Texture('img/enemy.png'),
    music: new ex.Sound('assets/snd/bossa_nova.mp3'),
