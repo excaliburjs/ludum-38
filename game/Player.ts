f9eeeb37ad80fe11e6fcdb87d4ec4d6f0c65c06f
Josh Edeen <josh.edeen@gmail.com>
2017-04-23 13:11:31 -0500

    Add placeholder player art

--- game/Player.ts
+++ game/Player.ts
@@ -4,12 +4,19 @@ class Player extends ex.Actor {
     */
    constructor(x, y) {
       super(x, y, Config.playerWidth, Config.playerHeight);
-      this.addDrawing(Resources.playerSheet);
    }
 
    public shoppingList : ShoppingList;
 
    public onInitialize(engine: ex.Engine) {
+      var playerSheet = new ex.SpriteSheet(Resources.playerSheet, 4, 1, 45, 45);
+      this.addDrawing('down', playerSheet.getSprite(0));
+      this.addDrawing('up', playerSheet.getSprite(1));
+      this.addDrawing('left', playerSheet.getSprite(2));
+      this.addDrawing('right', playerSheet.getSprite(3));
+      
+      this.setDrawing('down');
+
       this.collisionType = ex.CollisionType.Active;
       
       game.input.keyboard.on('hold', (keyHeld?: ex.Input.KeyEvent) => {
@@ -18,18 +25,22 @@ class Player extends ex.Actor {
                case ex.Input.Keys.Up :
                case ex.Input.Keys.W :
                   this.vel.setTo(this.vel.x, -Config.playerVel);
+                  player.setDrawing('up');
                   break;
                case ex.Input.Keys.Down :
                case ex.Input.Keys.S :
                   this.vel.setTo(this.vel.x, Config.playerVel);
+                  player.setDrawing('down');
                   break;
                case ex.Input.Keys.Left :
                case ex.Input.Keys.A :
                   this.vel.setTo(-Config.playerVel, this.vel.y);
+                  player.setDrawing('left');
                   break;
                case ex.Input.Keys.Right :
                case ex.Input.Keys.D :
                   this.vel.setTo(Config.playerVel, this.vel.y);
+                  player.setDrawing('right');
                   break;
             }
          }
