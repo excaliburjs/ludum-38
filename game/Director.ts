d36a359cfc9748d9368468da3c7557edee01f784
Alan Grgic <alan.grgic@genmills.com>
2017-04-24 18:39:57 -0500

    display playtime on game over dialog

--- game/Director.ts
+++ game/Director.ts
@@ -158,8 +158,10 @@ class Director extends ex.Actor {
       var elapsedSeconds = Math.round((endTime - Director.startTime) / 1000);
       var minutes = Math.floor(elapsedSeconds / 60);
       var seconds = elapsedSeconds - (minutes * 60);
+      var minuteText = (minutes === 1 ? "minute" : "minutes");
+      var secondText = (seconds === 1 ? "second" : "seconds");
       var timeMessage = "I shopped for " + minutes + " minutes, " + seconds + " seconds";
-      console.log(timeMessage); 
+      $('#game-over-playtime')[0].innerHTML = timeMessage; 
       game.stop();
       // reset bg music, in case player was being chased
       if (!Preferences.muteBackgroundMusic) {
