6458b5aaf91f02195894bd9b02c662fc2e155564
Travis-CI <travis@excaliburjs.com>
2017-04-24 23:08:38 +0000

    Deploying game for f1c292d1938b617b87e114f414c2292b1498cf17

--- game/game.js
+++ game/game.js
@@ -1368,7 +1368,8 @@ var Director = (function (_super) {
         var elapsedSeconds = Math.round((endTime - Director.startTime) / 1000);
         var minutes = Math.floor(elapsedSeconds / 60);
         var seconds = elapsedSeconds - (minutes * 60);
-        console.log("Play time: " + minutes + "min, " + seconds + "sec");
+        var timeMessage = "I shopped for " + minutes + " minutes, " + seconds + " seconds";
+        console.log(timeMessage);
         game.stop();
         // reset bg music, in case player was being chased
         if (!Preferences.muteBackgroundMusic) {
